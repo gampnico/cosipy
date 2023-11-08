1718859dd0cc10cb87cbda71938a09f0727d5c2c
gampnico <45390064+gampnico@users.noreply.github.com>
2023-11-08 14:17:02 +0100

    feat(roughness): get roughness for debris-covered snow

--- cosipy/modules/roughness.py
+++ cosipy/modules/roughness.py
@@ -1,38 +1,76 @@
-from constants import roughness_method, roughness_fresh_snow, \
-                      roughness_firn, roughness_ice, snow_ice_threshold, \
-                      aging_factor_roughness
+import constants
+from config import use_debris
+
 
 def updateRoughness(GRID):
-
-    roughness_allowed = ['Moelg12']
-    if roughness_method == 'Moelg12':
-        sigma = method_Moelg(GRID)
+    roughness_allowed = ["Moelg12"]
+    if constants.roughness_method == "Moelg12":
+        if not use_debris:
+            sigma = method_Moelg(GRID)
+        else:
+            sigma = method_Moelg_debris(GRID)
     else:
-        raise ValueError("Roghness method = \"{:s}\" is not allowed, must be one of {:s}".format(roughness_method,", ".join(roughness_allowed)))
+        error_message = (
+            f'Roughness method = "{constants.roughness_method}" is not allowed,',
+            f'must be one of {", ".join(roughness_allowed)}',
+        )
+        raise ValueError(" ".join(error_message))
 
     return sigma
 
 
 def method_Moelg(GRID):
-
-    """ This method updates the roughness length (Moelg et al 2009, J.Clim.)"""
-
-    # Get hours since the last snowfall
-    # First get fresh snow properties (height and timestamp)
-    fresh_snow_height, fresh_snow_timestamp, _  = GRID.get_fresh_snow_props()
-
-    # Get time difference between last snowfall and now
-    hours_since_snowfall = (fresh_snow_timestamp)/3600.0
+    """This method updates the roughness length (Moelg et al 2009, J.Clim.)"""
 
     # Check whether snow or ice
-    if (GRID.get_node_density(0) <= snow_ice_threshold):
+    if GRID.get_node_density(0) <= constants.snow_ice_threshold:
+        # Get hours since the last snowfall
+        # First get fresh snow properties (height and timestamp)
+        _, fresh_snow_timestamp, _ = GRID.get_fresh_snow_props()
 
+        # Get time difference between last snowfall and now
+        hours_since_snowfall = (fresh_snow_timestamp) / 3600.0
         # Roughness length linear increase from 0.24 (fresh snow) to 4 (firn) in 60 days (1440 hours); (4-0.24)/1440 = 0.0026
-        sigma = min(roughness_fresh_snow + aging_factor_roughness * hours_since_snowfall, roughness_firn)
-
+        sigma = min(
+            constants.roughness_fresh_snow
+            + constants.aging_factor_roughness * hours_since_snowfall,
+            constants.roughness_firn,
+        )
     else:
-
         # Roughness length, set to ice
-        sigma = roughness_ice
+        sigma = constants.roughness_ice
 
-    return (sigma / 1000)
+    return sigma / 1000
+
+
+def method_Moelg_debris(GRID) -> float:
+    """Update the roughness length for debris-covered glaciers.
+
+    Adapted from Moelg et al. (2009), J.Clim. The roughness length of
+    snow linearly increases from 0.24 (fresh snow) to 4 (firn) in 60
+    days (1440 hours) i.e. (4-0.24)/1440 = 0.0026.
+
+    Returns:
+        Surface roughness length, [mm]
+    """
+
+    # Check whether snow or ice
+    if GRID.get_node_ntype(0) == 1:
+        sigma = constants.roughness_debris
+    elif GRID.get_node_density(0) <= constants.snow_ice_threshold:
+        # First get fresh snow properties (height and timestamp)
+        _, fresh_snow_timestamp, _ = GRID.get_fresh_snow_props()
+
+        # Get time difference between last snowfall and now
+        hours_since_snowfall = (fresh_snow_timestamp) / 3600.0
+
+        sigma = min(
+            constants.roughness_fresh_snow
+            + constants.aging_factor_roughness * hours_since_snowfall,
+            constants.roughness_firn,
+        )
+    else:
+        # Roughness length, set to ice
+        sigma = constants.roughness_ice
+
+    return sigma / 1000
--- cosipy/tests/test_parameterisation_roughness.py
+++ cosipy/tests/test_parameterisation_roughness.py
@@ -1,21 +1,121 @@
 import numpy as np
+import pytest
 
 import constants
-from COSIPY import start_logging
-from cosipy.modules.roughness import updateRoughness
+import cosipy.modules.roughness as module_roughness
 
 
 class TestParamRoughness:
     """Tests methods for parametrising roughness."""
 
-    def test_updateRoughness(self, conftest_mock_grid, conftest_mock_grid_ice):
-        GRID = conftest_mock_grid
-        roughness = updateRoughness(GRID)
+    @pytest.mark.parametrize("arg_method", ["Wrong Method", "", None])
+    def test_updateRoughness_method_error(
+        self, monkeypatch, conftest_mock_grid, conftest_boilerplate, arg_method
+    ):
+        grid = conftest_mock_grid
+        valid_methods = ["Moelg12"]
+
+        conftest_boilerplate.patch_variable(
+            monkeypatch,
+            module_roughness.constants,
+            {"roughness_method": arg_method},
+        )
+        assert module_roughness.constants.roughness_method == arg_method
+        error_message = (
+            f'Roughness method = "{module_roughness.constants.roughness_method}"',
+            f"is not allowed, must be one of",
+            f'{", ".join(valid_methods)}',
+        )
+
+        with pytest.raises(ValueError, match=" ".join(error_message)):
+            module_roughness.updateRoughness(grid)
+
+    def test_method_moelg(
+        self,
+        monkeypatch,
+        conftest_boilerplate,
+        conftest_mock_grid,
+        conftest_mock_grid_ice,
+    ):
+        conftest_boilerplate.patch_variable(
+            monkeypatch, module_roughness, {"use_debris": False}
+        )
+        test_grid = conftest_mock_grid
+        compare_roughness = module_roughness.method_Moelg(test_grid)
+
         assert (
-            roughness >= constants.roughness_fresh_snow / 1000
-            and roughness <= constants.roughness_firn / 1000
+            constants.roughness_fresh_snow / 1000
+            <= compare_roughness
+            <= constants.roughness_firn / 1000
+        )
+
+        test_grid_ice = conftest_mock_grid_ice
+        ice_roughness = module_roughness.method_Moelg(test_grid_ice)
+        assert np.isclose(ice_roughness, constants.roughness_ice / 1000)
+
+    @pytest.mark.parametrize("arg_bury", [True, False])
+    def test_method_moelg_debris(
+        self,
+        monkeypatch,
+        conftest_boilerplate,
+        conftest_mock_grid,
+        conftest_mock_grid_ice,
+        arg_bury,
+    ):
+        conftest_boilerplate.patch_variable(
+            monkeypatch, module_roughness, {"use_debris": True}
+        )
+
+        test_grid = conftest_mock_grid
+        test_grid.add_fresh_debris(0.1, 2840.0, 273.15, 0.0)
+        if arg_bury:
+            test_grid.add_fresh_snow(0.1, 250.0, 270.0, 0.0)
+
+        compare_roughness = module_roughness.method_Moelg_debris(test_grid)
+        if arg_bury:
+            assert (
+                constants.roughness_fresh_snow / 1000
+                <= compare_roughness
+                <= constants.roughness_firn / 1000
+            )
+        else:
+            conftest_boilerplate.check_output(
+                compare_roughness, float, constants.roughness_debris / 1000
+            )
+
+        test_grid_ice = conftest_mock_grid_ice
+        ice_roughness = module_roughness.method_Moelg(test_grid_ice)
+        assert np.isclose(ice_roughness, constants.roughness_ice / 1000)
+
+    @pytest.mark.parametrize("arg_method", ["Moelg12"])
+    @pytest.mark.parametrize("arg_debris", [True, False])
+    def test_updateRoughness(
+        self,
+        monkeypatch,
+        conftest_boilerplate,
+        conftest_mock_grid,
+        conftest_mock_grid_ice,
+        arg_method,
+        arg_debris,
+    ):
+        conftest_boilerplate.patch_variable(
+            monkeypatch, module_roughness, {"use_debris": arg_debris}
+        )
+        conftest_boilerplate.patch_variable(
+            monkeypatch,
+            module_roughness.constants,
+            {"roughness_method": arg_method},
+        )
+        assert module_roughness.constants.roughness_method == arg_method
+
+        GRID = conftest_mock_grid
+        roughness = module_roughness.updateRoughness(GRID)
+        assert (
+            constants.roughness_fresh_snow / 1000
+            <= roughness
+            <= constants.roughness_firn / 1000
         )
 
         GRID_ice = conftest_mock_grid_ice
-        ice_roughness = updateRoughness(GRID_ice)
+        ice_roughness = module_roughness.updateRoughness(GRID_ice)
         assert np.isclose(ice_roughness, constants.roughness_ice / 1000)
