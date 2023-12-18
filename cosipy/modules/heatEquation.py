6f6c328414d08bb7658c5451f2de577a9a7bd549
gampnico <45390064+gampnico@users.noreply.github.com>
2023-12-18 14:18:26 +0100

    refactor(heatEquation): split heat equation to allow for debris

--- cosipy/modules/heatEquation.py
+++ cosipy/modules/heatEquation.py
@@ -2,50 +2,83 @@ import numpy as np
 from numba import njit
 
 @njit
-def solveHeatEquation(GRID, dt):
-    """ Solves the heat equation on a non-uniform grid
+def solveHeatEquation(GRID, dt: int):
+    """Solve the heat equation on a non-uniform grid.
 
-    dt  ::  integration time
-    
+    Args:
+        GRID (Grid): Glacier data mesh.
+        dt: Integration time.
     """
     # number of layers
     nl = GRID.get_number_layers()
 
-    # Define index arrays 
-    k   = np.arange(1,nl-1) # center points
-    kl  = np.arange(2,nl)   # lower points
-    ku  = np.arange(0,nl-2) # upper points
-    
-    # Get thermal diffusivity [m2 s-1]
-    K = np.asarray(GRID.get_thermal_diffusivity()) 
-    
-    # Get snow layer heights    
-    hlayers = np.asarray(GRID.get_height())
+    # Define index arrays
+    k = np.arange(1, nl - 1)  # center points
+    kl = np.arange(2, nl)  # lower points
+    ku = np.arange(0, nl - 2)  # upper points
+
+    temperatures = get_new_temperatures_cds2(
+        grid=GRID, lower=kl, middle=k, upper=ku, dt=dt
+    )
+
+    # Write results to GRID
+    GRID.set_temperature(temperatures)
+
+
+def get_new_temperatures_cds2(
+    grid, lower, central, upper, dt: int
+) -> np.ndarray:
+    """Solve heat equation using a 2nd-order central-difference scheme.
+
+    Args:
+        grid (Grid): Glacier data mesh.
+        lower: Sequence of lowermost spatial bounds (j-1).
+        central: Seuquence of spatial midpoints (j).
+        upper: Sequence of uppermost spatial bounds (j+1).
+
+    Returns:
+        Solved column temperatures.
+    """
+
+    total_layers = grid.get_number_layers()
+
+    k_mid = np.asarray(grid.get_thermal_diffusivity())
+    heights = np.asarray(grid.get_height())
 
     # Get grid spacing
-    diff = ((hlayers[0:nl-1]/2.0)+(hlayers[1:nl]/2.0))
-    hk = diff[0:nl-2]  # between z-1 and z
-    hk1 = diff[1:nl-1] # between z and z+1
-    
+    spacing = (heights[0 : total_layers - 1] / 2.0) + (
+        heights[1:total_layers] / 2.0
+    )
+    h_lo = spacing[0 : total_layers - 2]  # between z-1 and z
+    h_up = spacing[1 : total_layers - 1]  # between z and z+1
+
     # Get temperature array from grid|
-    T = np.array(GRID.get_temperature())
-    Tnew = T.copy()
-    
-    Kl = (K[1:nl-1]+K[2:nl])/2.0
-    Ku = (K[0:nl-2]+K[1:nl-1])/2.0
-    
+    temperature = np.array(grid.get_temperature())
+    temperature_new = temperature.copy()
+
+    k_lo = (k_mid[1 : total_layers - 1] + k_mid[2:total_layers]) / 2.0
+    k_up = (k_mid[0 : total_layers - 2] + k_mid[1 : total_layers - 1]) / 2.0
+
     stab_t = 0.0
     c_stab = 0.8
-    dt_stab  = c_stab * (min([min(diff[0:nl-2]**2/(2*Ku)),min(diff[1:nl-1]**2/(2*Kl))]))
-    
-    while stab_t < dt:
+    dt_stab = c_stab * (
+        min(
+            [
+                min(spacing[0 : total_layers - 2] ** 2 / (2 * k_up)),
+                min(spacing[1 : total_layers - 1] ** 2 / (2 * k_lo)),
+            ]
+        )
+    )
 
-        dt_use = np.minimum(dt_stab, dt-stab_t)
+    while stab_t < dt:
+        dt_use = np.minimum(dt_stab, dt - stab_t)
         stab_t = stab_t + dt_use
 
         # Update the temperatures
-        Tnew[k] += ((Kl*dt_use*(T[kl]-T[k])/(hk1)) - (Ku*dt_use*(T[k]-T[ku])/(hk))) / (0.5*(hk+hk1))
-        T = Tnew.copy()
-        
-    # Write results to GRID
-    GRID.set_temperature(T)
+        temperature_new[central] += (
+            (k_lo * dt_use * (temperature[lower] - temperature[central]) / (h_up))
+            - (k_up * dt_use * (temperature[central] - temperature[upper]) / (h_lo))
+        ) / (0.5 * (h_lo + h_up))
+        temperature = temperature_new.copy()
+
+    return temperature
--- cosipy/tests/test_parameterisation_heatEquation.py
+++ cosipy/tests/test_parameterisation_heatEquation.py
@@ -1,1 +1,36 @@
-from pytest import approx
+import numpy as np
+import pytest
+
+import cosipy.modules.heatEquation as module_heatEquation
+
+
+class TestParamHeatEquation:
+    """Tests heat equation solvers."""
+
+    melt_water = 1.0
+    timedelta = 3600
+
+    @pytest.mark.parametrize("arg_range", [True, False])
+    def test_get_new_temperatures_cds2(self, conftest_mock_grid, arg_range):
+        test_grid = conftest_mock_grid
+        test_temperatures = test_grid.get_temperature()
+        test_layers = test_grid.get_number_layers()
+
+        test_centre = np.arange(1, test_layers - 1)
+        test_lower = np.arange(2, test_layers)
+        test_upper = np.arange(0, test_layers - 2)
+        if not arg_range:  # test other iterables
+            test_centre = [*test_centre]
+            test_lower = [*test_lower]
+            test_upper = [*test_upper]
+
+        compare_temperatures = module_heatEquation.get_new_temperatures_cds2(
+            grid=test_grid,
+            lower=test_lower,
+            central=test_centre,
+            upper=test_upper,
+            dt=1,
+        )
+
+        assert isinstance(compare_temperatures, np.ndarray)
+        assert not np.array_equal(compare_temperatures, test_temperatures)
